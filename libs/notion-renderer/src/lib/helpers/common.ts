d5e7868410d164f3f14e32940d5e68d66c1e7725
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-10-17 00:33:33 +0530

    chore: updated readme

--- libs/notion-renderer/src/lib/helpers/common.ts
+++ libs/notion-renderer/src/lib/helpers/common.ts
@@ -2,6 +2,5 @@ import type { IAnnotations } from '../@types/text';
 
 export const hasAnnotations = (annotations: IAnnotations) => {
   const { color, ...rest } = annotations;
-
   return color !== 'default' || Object.values(rest).some((val) => Boolean(val));
 };
