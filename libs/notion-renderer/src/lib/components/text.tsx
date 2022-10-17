4a0187a100ce116cd7b59892b329bd2089a30cb9
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-10-17 15:24:05 +0530

    refactor: changes for text component

--- libs/notion-renderer/src/lib/components/text.tsx
+++ libs/notion-renderer/src/lib/components/text.tsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import { hasAnnotations } from '../helpers/common';
-
 import type { IAnnotations, IParagraphContent } from '../@types/text';
 
 export interface ITextProps {
@@ -8,35 +6,22 @@ export interface ITextProps {
 }
 
 const StyledSpan = styled.span<{ annotations: IAnnotations }>`
-  font-weight: ${({ annotations }) => (annotations.bold ? 'bold' : 'normal')};
-  font-style: ${({ annotations }) =>
-    annotations.italic ? 'italic' : 'normal'};
-  text-decoration: ${({ annotations }) =>
-    ` ${annotations.underline ? 'underline' : ''}
-      ${annotations.strikethrough ? 'line-through' : ''}
-    `};
-  ${({ annotations }) =>
-    annotations.code
-      ? 'font-family: monospace; background: var(--monospace_background); padding: 2px;'
-      : 'font-family: inherit;'}
-  ${({ annotations }) =>
-    annotations.color && annotations.color !== 'default'
-      ? `${
-          /_background/.test(annotations.color)
-            ? `background-color: var(--${annotations.color})`
-            : `color: var(--${annotations.color})`
-        }`
+  font-weight: ${({ annotations: { bold } }) => (bold ? 'bold' : 'normal')};
+  font-style: ${({ annotations: { italic } }) => (italic ? 'italic' : 'normal')};
+  text-decoration: ${({ annotations: { underline, strikethrough } }) =>
+    `${underline ? 'underline' : ''} ${strikethrough ? 'line-through' : ''}`.trim()};
+  ${({ annotations: { code } }) =>
+    code ? 'font-family: monospace; background: var(--monospace_background); padding: 2px;' : 'font-family: inherit;'}
+  ${({ annotations: { color } }) =>
+    color !== 'default'
+      ? `${/_background/.test(color) ? `background-color: var(--${color})` : `color: var(--${color})`}`
       : ''}
 `;
 
 const StyledParagraph = styled.p<{ color?: string }>`
   ${({ color }) =>
     color && color !== 'default'
-      ? `${
-          /_background/.test(color)
-            ? `background-color: var(--${color})`
-            : `color: var(--${color})`
-        }`
+      ? `${/_background/.test(color) ? `background-color: var(--${color})` : `color: var(--${color})`}`
       : ''}
 `;
 
@@ -48,17 +33,11 @@ export const NotionText = (props: ITextProps) => {
 
   return (
     <StyledParagraph color={paragraph.color}>
-      {paragraph.rich_text?.map((entity, idx) => {
-        if (hasAnnotations(entity.annotations)) {
-          return (
-            <StyledSpan key={`${id}_${idx}`} annotations={entity.annotations}>
-              {entity.text.content}
-            </StyledSpan>
-          );
-        }
-
-        return entity.text.content;
-      })}
+      {paragraph.rich_text?.map((entity, idx) => (
+        <StyledSpan key={`${id}_${idx}`} annotations={entity.annotations}>
+          {entity.text.content}
+        </StyledSpan>
+      ))}
     </StyledParagraph>
   );
 };
